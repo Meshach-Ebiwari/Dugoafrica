49b7b69944cb9866e3ab86e570eeb576c1141d89
Ebiwari Meshach <ebiwarimeshach@gmail.com>
2020-09-30 19:39:38 +0100

    new work

--- src/Row.js
+++ src/Row.js
@@ -4,10 +4,10 @@ import './Row.css';
 import YouTube from "react-youtube"; 
 import movieTrailer from "movie-trailer";
 
-// const base_url = "https://image.tmdb.org/t/p/original/";
-// function Row({ title, fetchUrl, isLargeRow}) {
-//     const [movies, setMovies] = useState([]);
-//     const [trailerUrl, setTrailerUrl] = useState("");
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow}) {
+    const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
 
 // A Snippet of code which runs based on a specific condition
